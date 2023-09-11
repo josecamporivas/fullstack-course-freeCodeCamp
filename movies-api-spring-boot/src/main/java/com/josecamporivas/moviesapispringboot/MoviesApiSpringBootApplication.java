7b5560460aa0d1f06fd2d581e15ab75bc5dfc950
Jose Campo Rivas <josecampor31@gmail.com>
2023-09-11 21:35:06 +0200

    backend spring boot project

--- movies-api-spring-boot/src/main/java/com/josecamporivas/moviesapispringboot/MoviesApiSpringBootApplication.java
+++ movies-api-spring-boot/src/main/java/com/josecamporivas/moviesapispringboot/MoviesApiSpringBootApplication.java
@@ -2,14 +2,19 @@ package com.josecamporivas.moviesapispringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class MoviesApiSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApiSpringBootApplication.class, args);
 	}
-	
+
+//	@GetMapping("/")
+//	public String apiRoot(){
+//		return "Hello world!";
+//	}
 }
---
+++ movies-api-spring-boot/src/main/java/com/josecamporivas/moviesapispringboot/ReviewService.java
@@ -0,0 +1,29 @@
+package com.josecamporivas.moviesapispringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String imdbId){
+        Review newReview = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(newReview))
+                .first();
+
+        return newReview;
+    }
+
+
+}
